5d056d53a2c51b74b38a97eff1adb9edfab33f50
vangelif <ev.fotiades@yahoo.com>
2024-01-23 20:06:35 +0200

    rubocop fixes

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,8 +1,13 @@
 class Users::RegistrationsController < Devise::RegistrationsController
+  before_action :configure_permitted_parameters, if: :devise_controller?
   skip_before_action :verify_authenticity_token
   respond_to :json
 
   protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: %i[name role])
+  end
 
   def after_sign_up_path_for(_resource)
     '/'
